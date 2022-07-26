e9c46bbbe05d13f27f6577be69fd60b22a010145
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-07-26 21:55:32 +0530

    new fix

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -63,12 +63,181 @@
 <script>
 import "animate.css";
 import { ref } from "vue";
-import {ProjectData} from './SmallComponents/ProjectData'
+import Project1 from "../../assets/images/Project1.jpg";
+import Project2 from "../../assets/images/Project2.jpg";
+import Project3 from "../../assets/images/Project3.jpg";
+import Project4 from "../../assets/images/Project4.jpg";
+import Project5 from "../../assets/images/Project5.jpg";
 
 
 export default {
   setup() {
-    const projectData = ref(ProjectData);
+    const projectData = ref([
+    {
+        ProjectId: "one",
+        ProjectTitle: "E - Recipes",
+        ProjectLink: "https://e-recipes.netlify.app/",
+        ProjectImage: Project1,
+        Date: "June 2022",
+        LanguagesUsed: [
+            {
+                languageName: "HTML5",
+                languageLink: "https://www.w3schools.com/html/",
+            },
+            {
+                languageName: "CSS3",
+                languageLink: "https://www.w3schools.com/css/",
+            },
+            {
+                languageName: "JAVASCRIPT",
+                languageLink: "https://www.w3schools.com/js/",
+            },
+            {
+                languageName: "TAILWIND",
+                languageLink: "https://tailwindcss.com/",
+            },
+            {
+                languageName: "REACT JS",
+                languageLink: "https://reactjs.org/",
+            },
+        ],
+        ProjectDescription:
+            "This Project is related to Food Recipes Finder where a user can search food and get the data related to it like summary, steps to cook , ingredients,health score,veg or nonveg , etc.It is Created using Spoonculer API",
+    },
+    {
+        ProjectId: "two",
+        ProjectTitle: "Waadu Blogs",
+        ProjectLink: "https://waadu-blog.netlify.app/",
+        ProjectImage: Project2,
+        Date: "May 2022",
+        LanguagesUsed: [
+            {
+                languageName: "HTML5",
+                languageLink: "https://www.w3schools.com/html/",
+            },
+            {
+                languageName: "CSS3",
+                languageLink: "https://www.w3schools.com/css/",
+            },
+            {
+                languageName: "JAVASCRIPT",
+                languageLink: "https://www.w3schools.com/js/",
+            },
+            {
+                languageName: "TAILWIND",
+                languageLink: "https://tailwindcss.com/",
+            },
+            {
+                languageName: "BOOTSTRAP 5",
+                languageLink:
+                    "https://getbootstrap.com/docs/5.0/getting-started/introduction/",
+            },
+            {
+                languageName: "REACT JS",
+                languageLink: "https://reactjs.org/",
+            },
+        ],
+        ProjectDescription:
+            "This Project is related to Blog creating website where we can create blogs with images using different categories.Its just a Design Project.",
+    },
+    {
+        ProjectId: "three",
+        ProjectTitle: "Image Editor",
+        ProjectLink: "https://waadu-image-editor.netlify.app/",
+        ProjectImage: Project4,
+        Date: "June 2022",
+        LanguagesUsed: [
+            {
+                languageName: "HTML5",
+                languageLink: "https://www.w3schools.com/html/",
+            },
+            {
+                languageName: "CSS3",
+                languageLink: "https://www.w3schools.com/css/",
+            },
+            {
+                languageName: "JAVASCRIPT",
+                languageLink: "https://www.w3schools.com/js/",
+            },
+            {
+                languageName: "TAILWIND",
+                languageLink: "https://tailwindcss.com/",
+            },
+            {
+                languageName: "REACT JS",
+                languageLink: "https://reactjs.org/",
+            },
+        ],
+        ProjectDescription:
+            "This Project is related to Image Editing. It has lots of options like blue,opacity,saturation,hue,rotation,contrast",
+    },
+    {
+        ProjectId: "four",
+        ProjectTitle: "Knowledge Earth",
+        ProjectLink: "https://knowledge-earth2k99.netlify.app/",
+        ProjectImage: Project3,
+        Date: "January 2022",
+        LanguagesUsed: [
+            {
+                languageName: "HTML5",
+                languageLink: "https://www.w3schools.com/html/",
+            },
+            {
+                languageName: "CSS3",
+                languageLink: "https://www.w3schools.com/css/",
+            },
+            {
+                languageName: "JAVASCRIPT",
+                languageLink: "https://www.w3schools.com/js/",
+            },
+            {
+                languageName: "TAILWIND",
+                languageLink: "https://tailwindcss.com/",
+            },
+            {
+                languageName: "REACT JS",
+                languageLink: "https://reactjs.org/",
+            },
+        ],
+        ProjectDescription:
+            "This Project is related to Wikipedia searching Api where a user can search things and then can read the whole content by redirecting to that wikipedia page and it also has a voice assistance",
+    },
+    {
+        ProjectId: "five",
+        ProjectTitle: "E-Quotes",
+        ProjectLink: "https://e-quotes-2099.netlify.app/",
+        ProjectImage: Project5,
+        Date: "july 2022",
+        LanguagesUsed: [
+            {
+                languageName: "HTML5",
+                languageLink: "https://www.w3schools.com/html/",
+            },
+            {
+                languageName: "CSS3",
+                languageLink: "https://www.w3schools.com/css/",
+            },
+            {
+                languageName: "JAVASCRIPT",
+                languageLink: "https://www.w3schools.com/js/",
+            },
+            {
+                languageName: "TAILWIND",
+                languageLink: "https://tailwindcss.com/",
+            },
+            {
+                languageName: "REACT JS",
+                languageLink: "https://reactjs.org/",
+            },
+            {
+                languageName: "FIREBASE",
+                languageLink: "https://firebase.google.com/",
+            },
+        ],
+        ProjectDescription:
+            "This Project is related to Quotes reading and writing app with likes/dislikes feature,comment section,downloading quotes,etc",
+    },
+]);
 
     return {
       projectData,
