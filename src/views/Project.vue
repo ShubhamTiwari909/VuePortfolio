3e6cec62df40229c44ea54f9b07ee8279b272836
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-07-26 21:18:26 +0530

    fixed commit

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -13,11 +13,11 @@
 
       <div :id="project.ProjectId">
         <div class="flex justify-between pt-4 pb-4 mx-3 md:mx-5">
-          <h1 class="text-2xl text-center">
+          <h1 class="text-2xl text-center text-indigo-900">
             {{ project.ProjectTitle }}
           </h1>
           <h1
-            class="text-xs md:text-sm text-slate-700 text-center rounded-lg mt-2"
+            class="text-xs md:text-sm text-indigo-900 text-center rounded-lg mt-2.5"
           >
             {{ project.Date }}
           </h1>
@@ -63,180 +63,12 @@
 <script>
 import "animate.css";
 import { ref } from "vue";
-import Project1 from "../assets/images/Project1.jpg";
-import Project2 from "../assets/images/Project2.jpg";
-import Project3 from "../assets/images/Project3.jpg";
-import Project4 from "../assets/images/Project4.jpg";
-import Project5 from "../assets/images/Project5.jpg";
+import {ProjectData} from './SmallComponents/ProjectData'
+
 
 export default {
   setup() {
-    const projectData = ref([
-      {
-        ProjectId: "one",
-        ProjectTitle: "E - Recipes",
-        ProjectLink: "https://e-recipes.netlify.app/",
-        ProjectImage: Project1,
-        Date: "June 2022",
-        LanguagesUsed: [
-          {
-            languageName: "HTML5",
-            languageLink: "https://www.w3schools.com/html/",
-          },
-          {
-            languageName: "CSS3",
-            languageLink: "https://www.w3schools.com/css/",
-          },
-          {
-            languageName: "JAVASCRIPT",
-            languageLink: "https://www.w3schools.com/js/",
-          },
-          {
-            languageName: "TAILWIND",
-            languageLink: "https://tailwindcss.com/",
-          },
-          {
-            languageName: "REACT JS",
-            languageLink: "https://reactjs.org/",
-          },
-        ],
-        ProjectDescription:
-          "This Project is related to Food Recipes Finder where a user can search food and get the data related to it like summary, steps to cook , ingredients,health score,veg or nonveg , etc.It is Created using Spoonculer API",
-      },
-      {
-        ProjectId: "two",
-        ProjectTitle: "Waadu Blogs",
-        ProjectLink: "https://waadu-blog.netlify.app/",
-        ProjectImage: Project2,
-        Date: "May 2022",
-        LanguagesUsed: [
-          {
-            languageName: "HTML5",
-            languageLink: "https://www.w3schools.com/html/",
-          },
-          {
-            languageName: "CSS3",
-            languageLink: "https://www.w3schools.com/css/",
-          },
-          {
-            languageName: "JAVASCRIPT",
-            languageLink: "https://www.w3schools.com/js/",
-          },
-          {
-            languageName: "TAILWIND",
-            languageLink: "https://tailwindcss.com/",
-          },
-          {
-            languageName: "BOOTSTRAP 5",
-            languageLink:
-              "https://getbootstrap.com/docs/5.0/getting-started/introduction/",
-          },
-          {
-            languageName: "REACT JS",
-            languageLink: "https://reactjs.org/",
-          },
-        ],
-        ProjectDescription:
-          "This Project is related to Blog creating website where we can create blogs with images using different categories.Its just a Design Project.",
-      },
-      {
-        ProjectId: "three",
-        ProjectTitle: "Image Editor",
-        ProjectLink: "https://waadu-image-editor.netlify.app/",
-        ProjectImage: Project4,
-        Date: "June 2022",
-        LanguagesUsed: [
-          {
-            languageName: "HTML5",
-            languageLink: "https://www.w3schools.com/html/",
-          },
-          {
-            languageName: "CSS3",
-            languageLink: "https://www.w3schools.com/css/",
-          },
-          {
-            languageName: "JAVASCRIPT",
-            languageLink: "https://www.w3schools.com/js/",
-          },
-          {
-            languageName: "TAILWIND",
-            languageLink: "https://tailwindcss.com/",
-          },
-          {
-            languageName: "REACT JS",
-            languageLink: "https://reactjs.org/",
-          },
-        ],
-        ProjectDescription:
-          "This Project is related to Image Editing. It has lots of options like blue,opacity,saturation,hue,rotation,contrast",
-      },
-      {
-        ProjectId: "four",
-        ProjectTitle: "Knowledge Earth",
-        ProjectLink: "https://knowledge-earth2k99.netlify.app/",
-        ProjectImage: Project3,
-        Date: "January 2022",
-        LanguagesUsed: [
-          {
-            languageName: "HTML5",
-            languageLink: "https://www.w3schools.com/html/",
-          },
-          {
-            languageName: "CSS3",
-            languageLink: "https://www.w3schools.com/css/",
-          },
-          {
-            languageName: "JAVASCRIPT",
-            languageLink: "https://www.w3schools.com/js/",
-          },
-          {
-            languageName: "TAILWIND",
-            languageLink: "https://tailwindcss.com/",
-          },
-          {
-            languageName: "REACT JS",
-            languageLink: "https://reactjs.org/",
-          },
-        ],
-        ProjectDescription:
-          "This Project is related to Wikipedia searching Api where a user can search things and then can read the whole content by redirecting to that wikipedia page and it also has a voice assistance",
-      },
-      {
-        ProjectId: "five",
-        ProjectTitle: "E-Quotes",
-        ProjectLink: "https://e-quotes-2099.netlify.app/",
-        ProjectImage: Project5,
-        Date: "july 2022",
-        LanguagesUsed: [
-          {
-            languageName: "HTML5",
-            languageLink: "https://www.w3schools.com/html/",
-          },
-          {
-            languageName: "CSS3",
-            languageLink: "https://www.w3schools.com/css/",
-          },
-          {
-            languageName: "JAVASCRIPT",
-            languageLink: "https://www.w3schools.com/js/",
-          },
-          {
-            languageName: "TAILWIND",
-            languageLink: "https://tailwindcss.com/",
-          },
-          {
-            languageName: "REACT JS",
-            languageLink: "https://reactjs.org/",
-          },
-          {
-            languageName: "FIREBASE",
-            languageLink: "https://firebase.google.com/",
-          },
-        ],
-        ProjectDescription:
-          "This Project is related to Quotes reading and writing app with likes/dislikes feature,comment section,downloading quotes,etc",
-      },
-    ]);
+    const projectData = ref(ProjectData);
 
     return {
       projectData,
---
+++ src/views/SmallComponents/SkillsData.js
@@ -0,0 +1,82 @@
+const SkillsData = [
+    {
+      SkillName: "HTML",
+      SkillPercentage: "85%",
+      Color: "bg-gradient-to-r from-red-900 via-red-600 to-red-400",
+      Type: "Frontend",
+    },
+    {
+      SkillName: "CSS",
+      SkillPercentage: "75%",
+      Color: "bg-gradient-to-r from-blue-900 via-blue-600 to-blue-400",
+      Type: "Frontend",
+    },
+    {
+      SkillName: "JAVASCRIPT",
+      SkillPercentage: "65%",
+      Color: "bg-gradient-to-r from-yellow-900 via-yellow-600 to-yellow-400",
+      Type: "Frontend",
+    },
+    {
+      SkillName: "TAILWIND",
+      SkillPercentage: "55%",
+      Color: "bg-gradient-to-r from-indigo-900 via-indigo-600 to-indigo-400",
+      Type: "Frontend",
+    },
+    {
+      SkillName: "BOOTSTRAP",
+      SkillPercentage: "85%",
+      Color: "bg-gradient-to-r from-purple-900 via-purple-600 to-purple-400",
+      Type: "Frontend",
+    },
+    {
+      SkillName: "REACT",
+      SkillPercentage: "55%",
+      Color: "bg-gradient-to-r from-cyan-900 via-cyan-600 to-cyan-400",
+      Type: "Frontend",
+    },
+    {
+      SkillName: "MySQL",
+      SkillPercentage: "55%",
+      Color: "bg-gradient-to-r from-gray-600 via-gray-400 to-gray-300",
+      Type: "Backend",
+    },
+    {
+      SkillName: "NODE",
+      SkillPercentage: "35%",
+      Color: "bg-gradient-to-r from-green-700 via-green-500 to-green-300",
+      Type: "Backend",
+    },
+      {
+      SkillName: "Mongo DB",
+      SkillPercentage: "40%",
+      Color: "bg-gradient-to-r from-green-300 via-green-400 to-green-300",
+      Type: "Backend",
+    },
+    {
+      SkillName: "MS Word",
+      SkillPercentage: "75%",
+      Color: "bg-gradient-to-r from-blue-600 via-blue-400 to-blue-500",
+      Type: "Other",
+    },
+    {
+      SkillName: "MS Powerpoint",
+      SkillPercentage: "60%",
+      Color: "bg-gradient-to-r from-red-600 via-red-400 to-red-300",
+      Type: "Other",
+    },
+    {
+      SkillName: "Vue",
+      SkillPercentage: "40%",
+      Color: "bg-gradient-to-r from-green-500 via-green-400 to-green-300",
+      Type: "Frontend",
+    },
+    {
+      SkillName: "Redux",
+      SkillPercentage: "60%",
+      Color: "bg-gradient-to-r from-gray-600 via-gray-700 to-gray-700",
+      Type: "Frontend",
+    },
+  ]
+
+  export {SkillsData}
