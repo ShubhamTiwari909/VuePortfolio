dd06377e3ce591dacfd7843048f1966987ce795d
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-07-28 18:05:22 +0530

    changed the background of contact section

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,77 +1,37 @@
 <template>
-  <div
-    class="grid align-items-center py-10 h-96"
-    v-bind:class="[darkMode ? 'bg-slate-200' : 'bg-slate-900']"
-  >
+  <div class="grid align-items-center py-10 h-96 bg-slate-900 bgImage">
     <div class="p-4">
-      <h1
-        class="text-center text-3xl pt-3 pb-6"
-        v-bind:class="[contactSection ? 'text-indigo-400' : 'text-indigo-800 ']"
-      >
-        Contacts
-      </h1>
-      <div
-        v-bind:class="[
-          contactSection ? showContact : hideContact,
-          darkMode ? 'bg-slate-900' : 'bg-slate-200',
-        ]"
-      >
+      <div v-bind:class="showContact">
         <a
           href="https://www.instagram.com/s_h.u_b.h_a.m_2k99/"
           target="_blank"
-          class="justify-self-center"
-          v-bind:class="[
-            contactSection ? 'animate__animated animate__backInLeft' : '',
-          ]"
+          class="justify-self-center animate__animated animate__backInLeft"
           ><i class="fa-brands fa-instagram"></i
         ></a>
         <a
           href="https://github.com/ShubhamTiwari909"
           target="_blank"
-          class="justify-self-center"
-          v-bind:class="[
-            contactSection ? 'animate__animated animate__backInLeft' : '',
-          ]"
+          class="justify-self-center animate__animated animate__backInLeft"
           ><i class="fa-brands fa-github"></i
         ></a>
         <a
           href="https://www.linkedin.com/in/shubham-tiwari-b7544b193/"
           target="_blank"
-          class="justify-self-center"
-          v-bind:class="[
-            contactSection ? 'animate__animated animate__backInDown' : '',
-          ]"
+          class="justify-self-center animate__animated animate__backInDown"
           ><i class="fa-brands fa-linkedin"></i
         ></a>
         <a
           href="https://wa.me/919627253516"
-          class="justify-self-center"
-          v-bind:class="[
-            contactSection ? 'animate__animated animate__backInRight' : '',
-          ]"
+          class="justify-self-center animate__animated animate__backInRight"
           ><i class="fa-brands fa-whatsapp"></i
         ></a>
         <a
           href="mailto:shubhmtiwri00@gmail.com"
-          class="justify-self-center"
-          v-bind:class="[
-            contactSection ? 'animate__animated animate__backInRight' : '',
-          ]"
+          class="justify-self-center animate__animated animate__backInRight"
           ><i class="fa-solid fa-envelope-circle-check"></i
         ></a>
       </div>
     </div>
-    <button
-      class="place-self-center mt-4 px-4 py-2 rounded-full border-2 focus:outline-none"
-      v-bind:class="[
-        contactSection
-          ? 'border-indigo-400 text-indigo-400'
-          : 'border-indigo-600 text-indigo-600',
-      ]"
-      @click="showContacts"
-    >
-      {{ contactSection ? "Hide" : "Show" }}
-    </button>
   </div>
 </template>
 
@@ -79,32 +39,22 @@
 export default {
   data() {
     return {
-      contactSection: false,
-      darkMode: true,
-      showContact: `showContact grid grid-cols-5 gap-2 md:gap-4 border-2 border-violet-400 text-violet-500 rounded-2xl
+      showContact: `bg-slate-200 grid grid-cols-5 gap-2 md:gap-4 border-2 border-violet-400 text-violet-500 rounded-2xl
         px-3 py-6 text-2xl md:text-4xl`,
-      hideContact: "hideContact",
     };
-  },
-  methods: {
-    showContacts() {
-      if (this.contactSection) {
-        this.contactSection = false;
-        this.darkMode = true;
-      } else {
-        this.contactSection = true;
-        this.darkMode = false;
-      }
-    },
   },
 };
 </script>
 
 <style scoped>
-.showContact {
-  visibility: visible;
+.bgImage {
+  background-image: url('https://media.istockphoto.com/photos/emails-with-blurred-city-lights-picture-id1134247993?k=20&m=1134247993&s=612x612&w=0&h=2m4qwm0V4V-a5QOP5n7tSC3njlxo1q7WsMBW82foJ7Y=');
+  background-size: cover;
 }
-.hideContact {
-  visibility: hidden;
+
+@media  screen and (max-width:600px){
+  .bgImage{
+    background-image: url('https://png.pngtree.com/thumb_back/fh260/background/20220313/pngtree-creative-synthesis-contact-future-technology-image_1059813.jpg')
+  }
 }
 </style>
--- src/views/SmallComponents/Summary.vue
+++ src/views/SmallComponents/Summary.vue
@@ -10,35 +10,35 @@
         skills related to the field of web development
       </p>
       <button
-        class="px-4 py-2 bg-transparent border-2 border-indigo-400 rounded-lg text-indigo-400 my-2"
+        class="px-4 py-2 bg-transparent border-2 border-indigo-400 rounded-lg text-indigo-400 my-2 focus:outline-none"
         @click="showHobbies = !showHobbies"
       >
         Hobbies
       </button>
-      <div v-show="showHobbies">
+      <div v-show="showHobbies" class="bg-slate-100 p-2 rounded-2xl mt-5">
         <ul class="list-none my-3 flex flex-wrap gap-2">
           <li
-            class="px-2 py-1 bg-slate-200 text-slate-800 rounded-xl border-2 border-indigo-400"
+            class="p-2 bg-slate-900 text-slate-200 rounded-xl border-2 border-indigo-400"
           >
             Badminton
           </li>
           <li
-            class="px-2 py-1 bg-slate-200 text-slate-800 rounded-xl border-2 border-indigo-400"
+            class="p-2 bg-slate-900 text-slate-200 rounded-xl border-2 border-indigo-400"
           >
             Chess
           </li>
           <li
-            class="px-2 py-1 bg-slate-200 text-slate-800 rounded-xl border-2 border-indigo-400"
+            class="p-2 bg-slate-900 text-slate-200 rounded-xl border-2 border-indigo-400"
           >
             Blogging
           </li>
           <li
-            class="px-2 py-1 bg-slate-200 text-slate-800 rounded-xl border-2 border-indigo-400"
+            class="p-2 bg-slate-900 text-slate-200 rounded-xl border-2 border-indigo-400"
           >
             Reading Books
           </li>
           <li
-            class="px-2 py-1 bg-slate-200 text-slate-800 rounded-xl border-2 border-indigo-400"
+            class="p-2 bg-slate-900 text-slate-200 rounded-xl border-2 border-indigo-400"
           >
             Mathematics
           </li>
